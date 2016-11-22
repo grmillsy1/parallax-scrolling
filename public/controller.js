66348cd22f5126e7e68589fcbc0c1956d65a59ba
Georgia Mills <georgee_123@hotmail.com>
2016-11-22 15:09:07 +0000

    added second example. using setPin to pin block

--- public/controller.js
+++ public/controller.js
@@ -1,13 +1,28 @@
-$(function() {
-  var controller = new ScrollMagic.Controller();
+// first function is to get the background colour to change at a trigger point.
 
-  var blockTween = new TweenMax.to('#block', 1.5, {
-    backgroundColor: "red"
-  });
-  var containerScene = new ScrollMagic.Scene({
-    triggerElement: '#container'
-  })
-  .setTween(blockTween)
-  .addIndicators()
-  .addTo(controller);
+// $(function() {
+//   var controller = new ScrollMagic.Controller();
+//
+//   var blockTween = new TweenMax.to('#block', 1.5, {
+//     backgroundColor: "red"
+//   });
+//   var containerScene = new ScrollMagic.Scene({
+//     triggerElement: '#container'
+//   })
+//   .setTween(blockTween)
+//   .addIndicators()
+//   .addTo(controller);
+// });
+
+//scond example is to pin an element
+
+$(function() {
+    var controller = new ScrollMagic.Controller();
+    var containerScene = new ScrollMagic.Scene({
+        triggerElement: '#container',
+        duration: 500
+    })
+    .setPin('#block')
+    .addIndicators()
+    .addTo(controller);
 });
